00c367f3a30d3cb5169f1dbca5cd3c669a91d6d6
viniciussobral <vinicius.araujo@bandtec.com.br>
2021-04-09 23:35:41 -0300

    Atualizando o Projeto

--- ProjetoMobile/app/src/main/java/com/example/urent/HomeChoice.kt
+++ ProjetoMobile/app/src/main/java/com/example/urent/HomeChoice.kt
@@ -1,11 +1,28 @@
 package com.example.urent
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
 
 class HomeChoice : AppCompatActivity() {
+
+    val temGaragem: Boolean = false
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_home_choice)
+    }
+
+    fun alugar(view: View) {
+
+        if (!temGaragem){
+
+            val cadastrarGaragem = Intent(this,CadastroGaragem::class.java)
+            startActivity(cadastrarGaragem)
+        }else{
+            val cadastrarAnuncio = Intent(this,CadastroAnuncio::class.java)
+            startActivity(cadastrarAnuncio)
+        }
     }
 }
--- ProjetoMobile/app/src/main/java/com/example/urent/Login.kt
+++ ProjetoMobile/app/src/main/java/com/example/urent/Login.kt
@@ -4,11 +4,33 @@ import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.View
+import android.widget.EditText
+import android.widget.Toast
 
 class Login : AppCompatActivity() {
+
+    lateinit var etEmail: EditText
+    lateinit var etSenha: EditText
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_login)
+
+        etEmail = findViewById(R.id.et_email)
+        etSenha = findViewById(R.id.et_senha)
+    }
+
+    fun entrar(view: View) {
+
+        if (etEmail.text.toString().equals("email") && etSenha.text.toString().equals("senha")){
+            val homeChoice = Intent(this,HomeChoice::class.java)
+            startActivity(homeChoice)
+
+            println("oi")
+        }else{
+            Toast.makeText(baseContext, "Senha e/ou Email Inválido(s)", Toast.LENGTH_SHORT).show()
+        }
+
     }
 
     fun sair(view: View) {
@@ -17,4 +39,6 @@ class Login : AppCompatActivity() {
         startActivity(telaInicio)
 
     }
+
+
 }
